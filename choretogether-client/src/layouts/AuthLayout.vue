6021b0311928f8cbbc042b3e4b982c9c9a42c74f
QuteViki <edacalderini14@gmail.com>
2026-08-30 12:26:17 +0200

    Update AuthLayout.vue

--- choretogether-client/src/layouts/AuthLayout.vue
+++ choretogether-client/src/layouts/AuthLayout.vue
@@ -1,9 +1,15 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <q-page class="flex flex-center bg-grey-2">
+      <q-page class="flex flex-center" :class="$q.dark.isActive ? 'bg-grey-10' : 'bg-grey-2'">
         <router-view />
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
+
+<script setup>
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+</script>
